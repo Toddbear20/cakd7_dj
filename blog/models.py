3a95675ad530f7aea9858c012c3c1a86eaf6c702
Toddbear20 <sch01483@gmail.com>
2022-08-05 09:48:30 +0900

    link-go

---
+++ blog/models.py
@@ -0,0 +1,18 @@
+from turtle import update
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=30)
+    content = models.TextField()
+
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return f'[{self.pk}]{self.title}'
+    
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
